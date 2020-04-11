659f84cd69eba06d6d72fde1b2d91c859dbd4ff5
bryan madrid <bryanfmadrid@gmail.com>
2020-04-11 15:27:41 +0800

    fix rendering of map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import useFetch from './utils/useFetch';
 import './App.css';
 
 function App() {
-  const position = [14.7123189, 121.09594745]
+  //const position = [14.7123189, 121.09594745]
   const [boundary, setBoundary] = useState(0)
-  console.log({ Map })
+  useEffect(() => {
+//fetch(`http://localhost:4001/api/reports/city/quezon-city/boundary/d57235ab-65bb-48b6-8cc6-430ae9525258`)
+  //.then(response => response.json())
+//.then((data) => {
+    //console.log({ data })
+    //setBoundary([14.7123189, 121.09594745])
+//})
+  
+setBoundary([14.7123189, 121.09594745])
+  }, [])
+
   return (
     <div className="App">
       <div className="columns map-container">
         <div id="map" className="column is-12">
-           <Map center={position} zoom={12}>
+           <Map className="map" center={boundary} zoom={13}>
             <TileLayer
-              //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              //url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
               attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             />
-            <Marker position={position}>
-              <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-            </Marker>
           </Map>
         </div>
       </div>
