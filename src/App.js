cf96dd6f3d2f9dc803b9da76005e2828de9c36ac
bryan madrid <bryanfmadrid@gmail.com>
2020-04-13 23:43:42 +0800

    Add initial tooltip report; import bulma for styling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import {
   Map,
   Marker,
   TileLayer,
+  Popup,
   Polygon
 } from 'react-leaflet';
 
+import 'bulma/css/bulma.css'
 import './App.css';
+
+import TooltipReport from './components/TooltipReport'
 import { REACT_APP_BASE_URL } from './environment'
 
 function App(props) {
@@ -17,13 +21,15 @@ function App(props) {
   const [boundary, setBoundary] = useState(0)
   const [marker, setMarkerPosition] = useState(null)
   const [polygon, setPolygon] = useState(0)
+  const [report, setReport] = useState({})
+  const [disabledInput, setDisabledInput] = useState(true)
   useEffect(() => {
     fetch(`${REACT_APP_BASE_URL}/api/reports/city/quezon-city`)
       .then(response => response.json())
       .then((data) => {
         const { result } = data
         setBarangays(data)
-        setBoundary([14.655012249999999, 121.03174620638671])
+        setBoundary([14.655012249999999, 121.03174620638671]) // Set Quezon City as center view
 
         const extractBarangay = pick(['barangays'])
         const transformData = compose( // TODO: Convert to reduce
@@ -45,12 +51,22 @@ function App(props) {
   }
 
   const getBrgyBoundary = (brgy) => {
+    // TODO: convert to async/await
     fetch(`${REACT_APP_BASE_URL}/api/reports/city/quezon-city/boundary/${brgy.id}`)
       .then(response => response.json())
       .then((data) => {
+        const lat = data.result.centre.lat
+        const lng = data.result.centre.lng
         setBoundary([data.result.centre.lat, data.result.centre.lng])
         setZoom(15)
         setPolygon(data.result.brgy_boundaries)
+
+      fetch(`${REACT_APP_BASE_URL}/api/reports/city/quezon-city/barangay/${brgy.id}/report`)
+        .then(response => response.json())
+        .then((data) => {
+          setMarkerPosition([lat, lng])
+          setReport(data)
+      })
     })
   }
 
@@ -59,9 +75,13 @@ function App(props) {
       <div className="columns">
         <div className="column is-12 map-container">
             <Map animate={true} className="map" center={boundary} zoom={zoom}>
-            <Polygon onClick={clickPolygon} color="#1890ff" positions={polygon} />
+            <Polygon color="#1890ff" positions={polygon} />
               {marker &&
-                <Marker position={marker}></Marker>
+                <Marker position={marker}>
+                  <Popup autoClose={false}>
+                    <TooltipReport {...report} />
+                  </Popup>
+                </Marker>
               }
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -83,7 +103,11 @@ function App(props) {
                   getRootProps,
                   }) => (
                     <div>
-                      <input {...getInputProps()} type="text" placeholder="Search Barangay"/>
+                      <div className="field">
+                        <div className="control">
+                          <input className="input is-primary is-large" {...getInputProps()} type="text" placeholder="Search Barangay"/>
+                        </div>
+                      </div>
                         {barangays && barangays.length > 0 && (
                           <ul {...getMenuProps()}>
                             {isOpen && barangays.filter(brgy => brgy.name.toLowerCase().includes(inputValue.toLowerCase())).map((brgy, index) => (
